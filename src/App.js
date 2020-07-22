626ac11f1b770f8041435a27337556a817d5d166
Protik Hosen <protik3380@gmail.com>
2020-07-23 02:37:09 +0600

    Row component design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./App.css";
-
+import Row from "./Row";
+import requests from "./request";
+import Banner from "./Banner";
 function App() {
   return (
     <div className="App">
-      <h1>Hey cleaver programmer !! lets build netflix clone today</h1>
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documenteries" fetchUrl={requests.fetchDocumentariesMovies} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
